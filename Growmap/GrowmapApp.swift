34858302b73adb28122c65901288425b913f2bda
harutakenaka <harutakenaka08@gmail.com>
2025-10-26 14:23:31 +0900

    Initial Commit

---
+++ Growmap/GrowmapApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrowmapApp.swift
+//  Growmap
+//
+//  Created by Haru Takenaka on 2025/10/26.
+//
+
+import SwiftUI
+
+@main
+struct GrowmapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
